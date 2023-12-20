bd7de8e0b5575965b88a91454c93b9e2b19e0f61
narlgut <98569363+narlgut@users.noreply.github.com>
2023-12-19 19:35:01 -0800

    Debug commit

--- src/pages/Keyboard.jsx
+++ src/pages/Keyboard.jsx
@@ -40,7 +40,7 @@ const Keyboard = () => {
             console.log(i);
             await axios.get(`https://east-dict-api.onrender.com/get/${words[i].substring(0, words[i].length - 1)}`)
             .then((res) => {
-                currChars.push(res.data.find((element) => element.etyNum == words[i][words[i].length - 1]).char);
+                currChars.push(res.data.find((element) => element.etyNum == parseInt(words[i][words[i].length - 1])).char);
             })
             .catch((err) => {
                 console.log(err);
