538aadee718da5242181cb25095f4ab31e9a70a6
narlgut <98569363+narlgut@users.noreply.github.com>
2023-12-19 19:07:12 -0800

    Debug commit

--- src/pages/Keyboard.jsx
+++ src/pages/Keyboard.jsx
@@ -43,6 +43,7 @@ const Keyboard = () => {
                 currChars.push(res.data.find((element) => element.etyNum == words[i][words[i].length - 1]).char);
             })
             .catch((err) => {
+                console.log("error");
                 if (words[i][0] == 'j') {
                     currChars.push("〻");
                 } else if (words[i][0] == '.') {
