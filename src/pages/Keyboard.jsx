fdef60dffe1fe45f9bfaed4262eb506d4b0a5bcd
narlgut <98569363+narlgut@users.noreply.github.com>
2023-12-19 19:25:44 -0800

    Debug commit

--- src/pages/Keyboard.jsx
+++ src/pages/Keyboard.jsx
@@ -33,17 +33,17 @@ const Keyboard = () => {
     }
 
     const ConvertToChars = async () => {
-        console.log("twice");
-
         const words = textArea.current.value.split(" ");
         const currChars = Array();
 
         for (let i = 0; i < words.length; ++i) {
             await axios.get(`https://east-dict-api.onrender.com/get/${words[i].substring(0, words[i].length - 1)}`)
             .then((res) => {
+                console.log("then");
                 currChars.push(res.data.find((element) => element.etyNum == words[i][words[i].length - 1]).char);
             })
             .catch((err) => {
+                console.log("catch");
                 if (words[i][0] == 'j') {
                     currChars.push("〻");
                 } else if (words[i][0] == '.') {
